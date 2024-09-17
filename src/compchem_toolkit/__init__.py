8cb3959f8cfbe9f6dfa8c59d4009ef6c6af83db6
Antonio M. Ferreira, Ph.D <tferreira@wavebreaktx.com>
2024-09-17 19:03:28 -0400

    fix: Added missing copyright

--- src/compchem_toolkit/__init__.py
+++ src/compchem_toolkit/__init__.py
@@ -1,1 +1,8 @@
+#  Copyright © 2024 Antonio M. Ferreira, Ph.D.
+#
+#  Project       : compchem-toolkit
+#  File          : __init__.py
+#  Last Modified : 7/2/2024
+#
+#  This software is covered by the MIT License (see LICENSE file for details).
 """compchem_toolkit package."""
